b4478b4993117ec9aa5c925a91a4d7955cc83e1e
panjilaksono <panjilakonompl@gmail.com>
2025-04-29 20:56:51 +0700

    adding dashboard controller

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -13,17 +13,17 @@ $routes->post('login', 'AuthController::login');
 $routes->post('logout', 'AuthController::logout');
 
 // user
-$routes->get('/user', 'Home::userDashboard', ['filter' => 'auth']);
-$routes->get('/member', 'Home::userMember', ['filter' => 'auth']);
+$routes->get('/user', 'DashboardController::userDashboard', ['filter' => 'auth']);
+$routes->get('/member', 'DashboardController::userMember', ['filter' => 'auth']);
 
 //admin
-$routes->get('/admin', 'Home::adminDashboard', ['filter' => 'auth']);
-$routes->get('/users', 'Home::users', ['filter' => 'auth']);       
-$routes->get('/membership', 'Home::adminMember', ['filter' => 'auth']);       
+$routes->get('/admin', 'DashboardController::adminDashboard', ['filter' => 'auth']);
+$routes->get('/users', 'DashboardController::users', ['filter' => 'auth']);       
+$routes->get('/membership', 'DashboardController::adminMember', ['filter' => 'auth']);       
 
-$routes->get('/users/create', 'Home::create', ['filter' => 'auth']); 
-$routes->post('/users/store', 'Home::store');                        
-$routes->get('/users/edit/(:any)', 'Home::edit/$1');                
-$routes->post('/users/update/(:any)', 'Home::update/$1');            
-$routes->get('/users/delete/(:any)', 'Home::delete/$1');            
+$routes->get('/users/create', 'DashboardController::create', ['filter' => 'auth']); 
+$routes->post('/users/store', 'DashboardController::store');                        
+$routes->get('/users/edit/(:any)', 'DashboardController::edit/$1');                
+$routes->post('/users/update/(:any)', 'DashboardController::update/$1');            
+$routes->get('/users/delete/(:any)', 'DashboardController::delete/$1');            
 
